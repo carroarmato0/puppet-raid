33244daba6bd437a44d262ccba3a0d356f6a6264
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2014-11-20 15:47:24 +0100

    Multiple symlinks for MegaCli64 as some hardcoded scripts expect different locations
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@ class  raid::config {
   case $raidcontroller {
     'megaraid': {
 
-      file {'/usr/bin/MegaCli64':
+      file {['/usr/bin/MegaCli64','/usr/sbin/MegaCli64']:
         ensure => link,
         target => '/opt/MegaRAID/MegaCli/MegaCli64',
       }
